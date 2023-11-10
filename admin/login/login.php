1967d2c64501ecd397aac435c0e1db0174490b59
Nhật Mặt Trăng <nhatcaca2004@gmail.com>
2023-11-10 12:58:32 +0700

    12/11

--- admin/login/login.php
+++ admin/login/login.php
@@ -1,11 +1,18 @@
 <?php
 session_start();
+include("../../model/pdo.php");
+include("../../model/dangnhap.php");
+
+
+if (isset($_SESSION["dangnhap_admin"])) {
+	header("Location: ../index.php");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-	<title>Login V18</title>
+	<title>Đăng nhập Admin</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!--===============================================================================================-->
@@ -33,29 +40,78 @@ session_start();
 </head>
 
 <body style="background-color: #666666;">
+	<?php
+	$sodienthoai = $pass = "";
+	$err_sodienthoai = $err_pass = "";
+	$err_pass_loi = "";
+
+	if (isset($_POST["submit"])) {
+		$sodienthoai = $_POST["sodienthoai"];
+		$pass = md5($_POST["pass"]);
+
+		$check = 0;
+		if (empty(trim($sodienthoai))) {
+			$err_sodienthoai = "Bạn chưa nhập trường này";
+			$check++;
+		} else {
+			if (!preg_match("/^0\d{9}$/", $sodienthoai)) {
+				$check++;
+				$err_sodienthoai = "Bạn phải nhập đúng định dạng số điện thoại";
+			}
+		}
+
+		if (empty(trim($pass))) {
+			$err_pass = "Bạn chưa nhập trường này";
+			$check++;
+		}
+
+		if ($check == 0) {
+			$resut = check_tk_one($sodienthoai, $pass);
+
+			if ($resut) {
+				$_SESSION["dangnhap_admin"] = $sodienthoai;
+				header("location: ../index.php");
+				// exit();
+			} else {
+				$err_pass_loi = "Bạn sai số điện thoại hoặc mật khẩu";
+			}
+		}
+
+
+
+	}
+
+
+	?>
 
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
-				<form class="login100-form validate-form">
+				<form class="login100-form validate-form" action="" method="post">
 					<span class="login100-form-title p-b-43">
-						Đăng nhập để vào admin
+						Đăng nhập Admin
 					</span>
-
 
 					<div class="wrap-input100 validate-input" data-validate="">
 						<input class="input100" type="tel" name="sodienthoai">
 						<span class="focus-input100"></span>
 						<span class="label-input100">Số điện thoại</span>
 					</div>
+					<p style="color:red;">
+						<?= $err_sodienthoai ?>
+					</p>
 
-
-					<div class="wrap-input100 validate-input" data-validate="Password is required">
+					<div class="wrap-input100 validate-input" data-validate="Vui lòng nhập mật khẩu">
 						<input class="input100" type="password" name="pass">
 						<span class="focus-input100"></span>
 						<span class="label-input100">Mật khẩu</span>
 					</div>
-
+					<p style="color:red;">
+						<?= $err_pass ?>
+					</p>
+					<p style="color:red;">
+						<?= $err_pass_loi ?>
+					</p>
 					<div class="flex-sb-m w-full p-t-3 p-b-32">
 						<div>
 							<a href="#" class="txt1">
@@ -64,28 +120,18 @@ session_start();
 						</div>
 					</div>
 
-
 					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
-							Login
+						<button type="submit" name="submit" class="login100-form-btn">
+							Đăng nhập
 						</button>
 					</div>
 
 					<div class="text-center p-t-46 p-b-20">
 						<span class="txt2">
-							or sign up using
+
 						</span>
 					</div>
 
-					<div class="login100-form-social flex-c-m">
-						<a href="#" class="login100-form-social-item flex-c-m bg1 m-r-5">
-							<i class="fa fa-facebook-f" aria-hidden="true"></i>
-						</a>
-
-						<a href="#" class="login100-form-social-item flex-c-m bg2 m-r-5">
-							<i class="fa fa-twitter" aria-hidden="true"></i>
-						</a>
-					</div>
 				</form>
 
 				<div class="login100-more" style="background-image: url('images/bg-01.jpg');">
--- model/dangnhap.php
+++ model/dangnhap.php
@@ -1,45 +1,8 @@
 <?php
-class dangnhap {
-    private $conn;
-
-    public function __construct() {
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "duan1";
-
-        $this->conn = new mysqli($servername, $username, $password, $dbname);
-        if ($this->conn->connect_error) {
-            die("Connection failed: " . $this->conn->connect_error);
-        }
-    }
-
-    public function checkLogin($phone, $password) {
-        $sql = "SELECT * FROM tbl_taikhoan WHERE sodienthoai='$phone' AND matkhau='$password'";
-        $result = $this->conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-            return $row;
-        } else {
-            return false;
-        }
-    }
-
-    public function checkPhoneNumberExists($phone) {
-        $sql = "SELECT * FROM tbl_taikhoan WHERE sodienthoai='$phone'";
-        $result = $this->conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function registerUser($name, $phone, $password) {
-        $sql = "INSERT INTO tbl_taikhoan (hoten, sodienthoai, matkhau, vaitro) VALUES ('$name', '$phone', '$password', 0)";
-        return $this->conn->query($sql);
-    }
+function check_tk_one($sodienthoai, $pass)
+{
+    $sql = "SELECT * FROM tbl_taikhoan WHERE sodienthoai = ? AND matkhau = ?";
+    $list_tk = pdo_query_one($sql, $sodienthoai, $pass);
+    return $list_tk;
 }
 ?>
