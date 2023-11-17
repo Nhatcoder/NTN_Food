c186044f67af95dd32302738472afcd44505fc9a
tuyendenthe <hvt910tranvantuyen@gmail.com>
2023-11-18 03:19:24 +0700

    18/11

---
+++ admin/trangthaidonhang/huydon.php
@@ -0,0 +1,73 @@
+<?php 
+  include "trangthaidonhang/title.php"
+?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="product-status-wrap">
+                <h4>Trạng thái đơn hàng</h4>
+                
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>STT</th>
+                            <th>Mã đơn hàng</th>
+                            <th>Họ tên khách hàng</th>
+                            <th>Địa chỉ</th>
+                            <th>Số điện thoại</th>
+                            <th>Ngày mua hàng</th>
+                            <th>Trạng thái</th>
+                            <th>Thao tác</th>
+                           
+                        </tr>
+                        <?php
+                        $rowCount = 0;
+                        foreach ($loaddonhanghuy as $key => $value) {
+                            extract($value);
+                            $rowCount++;
+                            ?>
+                        
+                            <tr>
+                                <td>
+                                    <?= $key + 1 ?>
+                                </td>
+                                <td>
+                                    <?= $ma_donhang ?>
+                                </td>
+                                <td>
+                                    <?= $hoten ?>
+                                </td>
+                                <td>
+                                    <?= $diachi ?>
+                                </td>
+                                <td>
+                                    <?= $sodienthoai ?>
+                                </td>
+                                <td>
+                                    <?= $ngaymua ?>
+                                </td>
+                                <td>
+                                    <?= $tentrangthai ?>
+                                </td>
+                                <td>
+                                    
+                                    <a 
+                                        href="index.php?act=chitietdonhang&iddh=<?= $ma_donhang ?>">
+                                        <button data-toggle="tooltip" title="" class="pd-setting-ed"
+                                            data-original-title="Chi tiết"><i class="fa fa-pencil-square-o"
+                                                aria-hidden="true"></i></button>
+                                    </a>
+                                </td>
+                            </tr>
+
+                            
+                        <?php } ?>
+                    </tbody>
+                </table>
+                <?php if($rowCount == 0){
+                                echo "<h3>Danh sách trống</h3>";
+                            } ?>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ admin/trangthaidonhang/suatrangthai.php
@@ -0,0 +1,52 @@
+<?php
+include("title.php");
+
+?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="form_them">
+                <h4>Cập nhật trạng thái</h4>
+                <form action="" method="post" enctype="multipart/form-data">
+                    <input type="hidden" name="ma_donhang" value="<?= $ma_donhang ?>">
+                    
+                    
+
+                    <div class="chosen-select-single mg-b-20 mb-3">
+                        <label class="form-label">Trạng thái</label>
+                        <select name="id_trangthai" class="select2_demo_3 form-control">
+                            <?php
+
+                            foreach ($trangthai as $key => $value) {
+                                $selected = ($id_trangthai == $value['id_trangthai']) ? "selected" : "";
+                                ?>
+                                <option <?= $selected ?> value="<?= $value["id_trangthai"] ?>">
+                                    <?= $value["tentrangthai"] ?>
+                                </option>
+                            <?php } ?>
+                        </select>
+                     
+                    </div>
+
+                    
+
+                    
+
+                    <div class="mb-3 form_btn form-check">
+                        <input class="btn btn-primary mr-3 text-left" type="submit" name="capnhatdonhang" value="CẬP NHẬT MỚI"
+                            required>
+                        <a href="index.php?act=quanlydonhang"><input class="btn btn-success text-left" type="button"
+                                value="DANH SÁCH"></a>
+                    </div>
+                </form>
+
+                <?php
+                if (isset($thongbao) && $thongbao != "") {
+                    echo '<div class="alert alert-success" role="alert">' . $thongbao . '</div>';
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
